cd908c8bfc73b9eaae87d169afe7fd9cbce08cd2
“your_username” <67698619@qq.com>
2018-06-07 18:22:43 +0800

    假装完工

--- src/demo/Move.java
+++ src/demo/Move.java
@@ -5,7 +5,7 @@ import map.Player;
 
 public class Move {
     private int[][] Map;
-    private String moveAction="right";
+    private String moveAction="up";
     private String fire="right";
     private Player player;
     int dx[]={1,0,-1,0};
@@ -31,7 +31,7 @@ public class Move {
         int fireLock=0;
         for(int j=0;j<width;j++)
         {
-            if(Map[player.getY()][j]==6)
+            if(Map[player.getY()][j]==6||Map[player.getY()][j]==3)
             {
                 if(j<player.getX())
                 {
@@ -47,7 +47,7 @@ public class Move {
         }
         for(int j=0;j<height;j++)
         {
-            if(Map[j][player.getX()]==6)
+            if(Map[j][player.getX()]==6||Map[j][player.getX()]==3)
             {
                 if(j<player.getY())
                 {
@@ -67,19 +67,28 @@ public class Move {
 
             if(i==0)
             {
-                if(player.getX()+1<width){
+                if(player.getX()+1<width&& Map[player.getY()][player.getX()+1]!=5){
+                    for(int o=player.getX();o<width;o++)
+                    {
+                        if(Map[player.getY()][o]==4||Map[player.getY()][o]==8)
+                        {
+                            if(Map[player.getY()][player.getX()+1]==2||Map[player.getY()][player.getX()+1]==7)break;
+                            if(Map[player.getY()][player.getX()+1]==1||Map[player.getY()][player.getX()+1]==0)
+                            {
+                                if(fireLock==0)
+                                {
+                                    fire="right";
+                                    moveAction="right";
+                                    ok=1;
+                                }
+                            }
+                        }
+                    }
                     if(Map[player.getY()][player.getX()+1]==1)
                     {   if(fireLock==0)
                         fire="right";
 
                         moveAction="right";
-                        ok=1;
-                    }
-                    if(ok!=1&&(Map[player.getY()][player.getX()+1]==3||(player.getX()+2<width&&Map[player.getY()][player.getX()+2]==3)||(player.getX()+3<width&&Map[player.getY()][player.getX()+3]==3)))
-                    {if(fireLock==0)
-                        fire="right";
-
-                        moveAction="down";
                         ok=1;
                     }
                     if(ok!=1&&Map[player.getY()][player.getX()+1]==2)
@@ -94,7 +103,7 @@ public class Move {
                         }
                     }
 
-                    if(ok!=1&&(Map[player.getY()][player.getX()+1]==0||Map[player.getY()][player.getX()+1]==4||Map[player.getY()][player.getX()+1]==8))
+                    if(ok!=1&&Map[player.getY()][player.getX()+1]==0)
                     {if(fireLock==0)
                         fire="right";
                         moveAction="right";
@@ -102,9 +111,25 @@ public class Move {
                     }
                 }
             }
-            if(i==1)
+            if(i==3)
             {
-                if(player.getY()+1<height){
+                if(player.getY()+1<height&&Map[player.getY()+1][player.getX()]!=5){
+                    for(int o=player.getY();o<height;o++)
+                    {
+                        if(Map[o][player.getX()]==4||Map[o][player.getX()]==8)
+                        {
+                            if(Map[player.getY()+1][player.getX()]==2||Map[player.getY()+1][player.getX()]==7)break;
+                            if(Map[player.getY()+1][player.getX()]==1||Map[player.getY()+1][player.getX()]==0)
+                            {
+                                if(fireLock==0)
+                                {
+                                    fire="down";
+                                    moveAction="down";
+                                    ok=1;
+                                }
+                            }
+                        }
+                    }
                     if(Map[player.getY()+1][player.getX()]==1)
                     {if(fireLock==0)
                         fire="down";
@@ -139,7 +164,23 @@ public class Move {
             }
             if(i==2)
             {
-                if(player.getX()-1>=0){
+                if(player.getX()-1>=0&&Map[player.getY()][player.getX()-1]!=5){
+                    for(int o=player.getX();o>=0;o--)
+                    {
+                        if(Map[player.getY()][o]==4||Map[player.getY()][o]==8)
+                        {
+                            if(Map[player.getY()][player.getX()-1]==2||Map[player.getY()][player.getX()-1]==7)break;
+                            if(Map[player.getY()][player.getX()-1]==1||Map[player.getY()][player.getX()-1]==0)
+                            {
+                                if(fireLock==0)
+                                {
+                                    fire="left";
+                                    moveAction="left";
+                                    ok=1;
+                                }
+                            }
+                        }
+                    }
                     if(Map[player.getY()][player.getX()-1]==1)
                     {   if(fireLock==0)
                         fire="left";
@@ -172,9 +213,25 @@ public class Move {
                     }
                 }
             }
-            if(i==3)
+            if(i==1)
             {
-                if(player.getY()-1>=0){
+                if(player.getY()-1>=0&&Map[player.getY()-1][player.getX()]!=5){
+                    for(int o=player.getY();o>=0;o--)
+                    {
+                        if(Map[o][player.getX()]==4||Map[o][player.getX()]==8)
+                        {
+                            if(Map[player.getY()-1][player.getX()]==2||Map[player.getY()-1][player.getX()]==7)break;
+                            if(Map[player.getY()-1][player.getX()]==1||Map[player.getY()-1][player.getX()]==0)
+                            {
+                                if(fireLock==0)
+                                {
+                                    fire="up";
+                                    moveAction="up";
+                                    ok=1;
+                                }
+                            }
+                        }
+                    }
                     if(Map[player.getY()-1][player.getX()]==1)
                     {   if(fireLock==0)
                         fire="up";
